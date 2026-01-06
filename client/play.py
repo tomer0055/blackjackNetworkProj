eab3fab97fac41b23a1e4c3c2eb8b141c3f7c2e9
eden yaakobi <edenyaa@post.bgu.ac.il>
2026-01-06 15:45:25 +0200

    fixed round print and compile error

--- client/play.py
+++ client/play.py
@@ -1,4 +1,4 @@
-from network_module.msg_format import msgFormatHandler
+from network_module.msg_format import MsgFormatHandler
 
 class Play:
     def __init__(self, clientNet, view):
@@ -10,25 +10,25 @@ class Play:
 
     def start_game(self,roundsNum):
         self.view.show_game_start(roundsNum)
-        for _ in range(roundsNum):
-            self.view.show_round_start(roundsNum)
+        for round_index in range(1, roundsNum + 1):
+            self.view.show_round_start(round_index)
             self.play_single_round()
         self.view.end_game(self.wins, self.losses, self.ties)    
         
     def play_single_round(self):
         while True:
-            # 1. Receive payload from server (blocking)
+            # Receive payload from server
             data = self.clientNet.receive_tcp()
 
-            # 2. Parse payload using protocol handler
-            parsed = msgFormatHandler.client_receive_payload_parse(data)
+            # Parse payload using protocol handler
+            parsed = MsgFormatHandler.client_receive_payload_parse(data)
             if parsed is None:
                 self.view.show_error("Received invalid payload, ignoring")
                 continue
 
             round_result, card_rank, card_suit = parsed
 
-            # 3. Show received card (if exists)
+            # Show received card 
             if card_rank != 0:
                 self.view.show_received_card(card_rank, card_suit)
 
@@ -41,7 +41,7 @@ class Play:
             decision = self.view.ask_player_decision()
 
             # 6. Send decision to server
-            packet = msgFormatHandler.to_payload_format_client(decision)
+            packet = MsgFormatHandler.to_payload_format_client(decision)
             self.clientNet.send(packet)
 
                 
