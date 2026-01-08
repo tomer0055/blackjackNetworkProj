5e8fddf8345a783fb5cf6796c0c027fd39670538
tomer <tomer0033265@gmail.com>
2026-01-08 19:34:11 +0200

    finish just with out comment down

--- server/gameMan.py
+++ server/gameMan.py
@@ -1,3 +1,4 @@
+import threading
 from server.table import table
 from server.player import player
 import socket
@@ -10,11 +11,16 @@ class gameManager:
         pass
     @staticmethod
     def start_game(conn,addr):
+        tcp_cl = None
 
         try:
             print(f"Starting game with client at {addr}")
             tcp_cl = TcpClient(conn, addr)
             num_rounds,client_team_name = tcp_cl.recv_request()
+            if num_rounds is None or client_team_name is None:
+                print(f"Invalid initial request from client at {addr}. Ending game.")
+                return
+            
             p = player(client_team_name,tcp_cl)
             tb = table(p)
             
@@ -26,10 +32,21 @@ class gameManager:
             print(f"Error during game with client at {addr}: {e}")
         finally:
             print(f"Ending game with client at {addr}")
-            tcp_cl.close()
+            if tcp_cl is not None:
+                tcp_cl.close()
+            else:
+                conn.close()
         
+    @staticmethod
+    def game_loop(sc):
+        while True:
+            conn, addr = sc.accept()
+            print(f"New TCP connection from {addr}")
 
-    
+            print(f"Starting game manager...{conn},{addr}")
+            th=threading.Thread(target=gameManager.start_game, args=(conn,addr), daemon=True).start()
+            
+     
 
 if __name__ == "__main__":
     print("Server started")
@@ -41,17 +58,15 @@ if __name__ == "__main__":
     print(f"Server listening on TCP port {tcp_port}")
     try:
         sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sc.bind(("", tcp_port))    
         sc.listen()
+        gameManager.game_loop(sc)
     except Exception as e:
         print(f"Failed to start TCP server on port {tcp_port}: {e}")
         exit(1)
-
-    while True:
-        conn, addr = sc.accept()
-        print(f"New TCP connection from {addr}")
-        print(f"Starting game manager...{conn},{addr}")
-        gameManager.start_game(conn,addr)
+    
+    
                 
 
 
